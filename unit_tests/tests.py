08bbaeb4e39df367ececc14a5fa43b6ca33584ed
Sommos <Sommosgaming@gmail.com>
2023-07-28 00:32:31 +0100

    updated unit tests in tests.py

--- unit_tests/tests.py
+++ unit_tests/tests.py
@@ -1,23 +1,41 @@
+import unittest
 import main
 
-def test_binary_search():
-    arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    assert main.binary_search(arr, 1) == 0
-    assert main.binary_search(arr, 5) == 4
-    assert main.binary_search(arr, 10) == 9
-    assert main.binary_search(arr, 11) == -1
-    assert main.binary_search(arr, 0) == -1
+class TestBinarySearch(unittest.TestCase):
+    def test_empty_array(self):
+        arr = []
+        self.assertEqual(main.binary_search(arr, 1), -1)
 
-    arr = [1, 3, 5, 7, 9]
-    assert main.binary_search(arr, 1) == 0
-    assert main.binary_search(arr, 5) == 2
-    assert main.binary_search(arr, 9) == 4
-    assert main.binary_search(arr, 11) == -1
-    assert main.binary_search(arr, 0) == -1
+    def test_single_element_array(self):
+        arr = [1]
+        self.assertEqual(main.binary_search(arr, 1), 0)
+        self.assertEqual(main.binary_search(arr, 2), -1)
 
-    arr = [2, 4, 6, 8, 10]
-    assert main.binary_search(arr, 2) == 0
-    assert main.binary_search(arr, 6) == 2
-    assert main.binary_search(arr, 10) == 4
-    assert main.binary_search(arr, 11) == -1
-    assert main.binary_search(arr, 0) == -1
+    def test_even_length_array(self):
+        arr = [1, 2, 3, 4]
+        self.assertEqual(main.binary_search(arr, 1), 0)
+        self.assertEqual(main.binary_search(arr, 2), 1)
+        self.assertEqual(main.binary_search(arr, 3), 2)
+        self.assertEqual(main.binary_search(arr, 4), 3)
+        self.assertEqual(main.binary_search(arr, 5), -1)
+
+    def test_odd_length_array(self):
+        arr = [1, 2, 3, 4, 5]
+        self.assertEqual(main.binary_search(arr, 1), 0)
+        self.assertEqual(main.binary_search(arr, 2), 1)
+        self.assertEqual(main.binary_search(arr, 3), 2)
+        self.assertEqual(main.binary_search(arr, 4), 3)
+        self.assertEqual(main.binary_search(arr, 5), 4)
+        self.assertEqual(main.binary_search(arr, 6), -1)
+
+    def test_duplicate_elements(self):
+        arr = [1, 2, 2, 3, 4, 4, 4, 5]
+        self.assertEqual(main.binary_search(arr, 1), 0)
+        self.assertEqual(main.binary_search(arr, 2), 1)
+        self.assertEqual(main.binary_search(arr, 3), 3)
+        self.assertEqual(main.binary_search(arr, 4), 4)
+        self.assertEqual(main.binary_search(arr, 5), 7)
+        self.assertEqual(main.binary_search(arr, 6), -1)
+
+if __name__ == '__main__':
+    unittest.main()
